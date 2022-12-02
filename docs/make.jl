87865e4464e44f96a3a0fd31cd3da1c6b5aff1c7
LouisBouvier <lbouvier975@gmail.com>
2022-12-02 10:49:49 +0100

    remove tutorial with GLPK from the doc

--- docs/make.jl
+++ docs/make.jl
@@ -4,9 +4,9 @@ using Literate
 
 DocMeta.setdocmeta!(SingleMachineScheduling, :DocTestSetup, :(using SingleMachineScheduling); recursive=true)
 
-expe_jl_file = joinpath(dirname(@__DIR__), "test", "experiments.jl")
-expe_md_dir = joinpath(@__DIR__, "src")
-Literate.markdown(expe_jl_file, expe_md_dir; documenter=true, execute=true)
+# expe_jl_file = joinpath(dirname(@__DIR__), "test", "experiments.jl")
+# expe_md_dir = joinpath(@__DIR__, "src")
+# Literate.markdown(expe_jl_file, expe_md_dir; documenter=true, execute=true)
 
 makedocs(;
     modules=[SingleMachineScheduling],
@@ -22,7 +22,7 @@ makedocs(;
         "Home" => "index.md",
         "Exact MILP" => "milp.md",
         "Heuristics" => "heuristics.md",
-        "Experiments" => "experiments.md",
+        # "Experiments" => "experiments.md",
         "API" => "api.md"
     ],
 )
